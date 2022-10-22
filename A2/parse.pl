445b80bbe594f457a700cbebd128e5b149fafc37
tanyagupta1 <tanya19119@iiitd.ac.in>
2022-10-22 12:29:27 +0530

    parse csv to KB

---
+++ A2/parse.pl
@@ -0,0 +1,25 @@
+:- [library(csv)].
+
+:- dynamic edge/3.
+:- dynamic column_keys/1.
+
+prepare_db(File) :-
+    retractall(column_keys(_)),
+    retractall(edge(_,_,_)),
+    forall(read_row(File, Row), store_row(Row)).
+
+store_row(Row) :-
+    Row =.. [row|Cols],
+    (   column_keys(ColKeys)
+    ->  Cols = [RowKey|Samples],
+        maplist(store_sample(RowKey), ColKeys, Samples)
+    ;   assertz(column_keys(Cols))
+    ).
+
+store_sample(RowKey, ColKey, Sample) :-
+    not(RowKey=ColKey),assertz(edge(RowKey, ColKey, Sample)).
+
+read_row(File, Row) :-
+    csv_read_file_row(File, Row,[]).
+
+% src: https://stackoverflow.com/questions/22591030/prolog-read-a-csv-file-and-make-a-predicate-findall?rq=1
